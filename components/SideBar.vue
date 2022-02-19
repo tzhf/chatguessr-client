1fba5039f10d766d0e7566230594d4712047496d
unknown <theo.zahaf@gmail.com>
2022-02-19 10:53:35 +0100

    twitter section

--- components/SideBar.vue
+++ components/SideBar.vue
@@ -36,6 +36,36 @@
 			</section>
 
 			<section class="card m-2 text-center">
+				<h2>Follow us on Twitter</h2>
+
+				<a id="twitterIcon" href="https://www.twitter.com/chatguessr" target="_blank"
+					><svg
+						class="svg-inline--fa fa-twitter fa-w-16"
+						aria-hidden="true"
+						data-prefix="fab"
+						data-icon="twitter"
+						role="img"
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 512 512"
+						data-fa-i2svg=""
+					>
+						<path
+							fill="currentColor"
+							d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"
+						></path>
+					</svg>
+				</a>
+			</section>
+
+			<section class="card m-2 text-center">
+				<h2>Contact</h2>
+				<p>
+					For any inquiries:<br />
+					<a href="mailto:chatguessr@gmail.com">chatguessr@gmail.com</a>
+				</p>
+			</section>
+
+			<section class="card m-2 text-center">
 				<h2>Help us</h2>
 				<p>
 					ChatGuessr is free, however API and hosting costs are increasing each month.<br />
@@ -45,14 +75,6 @@
 					<input type="hidden" name="hosted_button_id" value="HLHT6B9ZF8DXC" />
 					<button name="submit" class="btn bordered-success mt-2">Donate</button>
 				</form>
-			</section>
-
-			<section class="card m-2 text-center">
-				<h2>Contact</h2>
-				<p>
-					For any inquiries:<br />
-					<a href="mailto:chatguessr@gmail.com">chatguessr@gmail.com</a>
-				</p>
 			</section>
 		</div>
 	</aside>
@@ -107,5 +129,25 @@ h4 {
 }
 .download__title {
 	margin-bottom: -0.1rem;
+}
+
+#twitterIcon svg {
+	width: 50px;
+}
+
+#twitterIcon {
+	outline: none;
+	padding: 2px 0;
+	color: var(--clr-primary);
+	opacity: 0.8;
+	text-decoration: none;
+	background: none;
+}
+#twitterIcon:hover {
+	opacity: 0.9;
+}
+
+#twitterIcon:focus {
+	opacity: 1;
 }
 </style>
