2f009650652388642bb1dee2aa560a23a086ae61
ReAnna <i-am-anna@outlook.com>
2022-11-05 17:00:22 +0100

    v2.1.1

--- components/SideBar.vue
+++ components/SideBar.vue
@@ -14,7 +14,7 @@
 
 			<section class="card m-2 text-center">
 				<h2>Download</h2>
-				<h5>version 2.1.0</h5>
+				<h5>version 2.1.1</h5>
 				<div class="flex mx-2">
 					<button @click="winDownload" class="btn btn-sep bordered-success icon-win">Windows</button>
 					<button @click="macOSDownload" class="btn btn-sep bordered-success icon-mac">macOS</button>
@@ -85,16 +85,16 @@
 export default {
 	methods: {
 		winDownload() {
-			window.open("https://github.com/tzhf/chatguessr/releases/download/v2.1.0/ChatGuessr-2.1.0.Setup.exe", "_blank");
+			window.open("https://github.com/tzhf/chatguessr/releases/download/v2.1.1/ChatGuessr-2.1.1.Setup.exe", "_blank");
 		},
 		winDownloadOld() {
 			window.open("https://github.com/tzhf/chatguessr/releases/download/v1.0.9/Chatguessr-Setup-1.0.9.exe", "_blank");
 		},
 		macOSDownload() {
-			window.open("https://github.com/tzhf/chatguessr/releases/download/v2.1.0/ChatGuessr-darwin-x64-2.1.0.zip", "_blank");
+			window.open("https://github.com/tzhf/chatguessr/releases/download/v2.1.1/ChatGuessr-darwin-x64-2.1.1.zip", "_blank");
 		},
 		linuxDownload() {
-			window.open("https://github.com/tzhf/chatguessr/releases/download/v2.1.0/chatguessr_2.1.0_amd64.deb", "_blank");
+			window.open("https://github.com/tzhf/chatguessr/releases/download/v2.1.1/chatguessr_2.1.1_amd64.deb", "_blank");
 		},
 	},
 };
