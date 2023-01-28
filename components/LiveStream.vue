ae94618c26d38d95c46a8bbff1eed46c40013d43
tzhf <theo.zahaf@gmail.com>
2023-01-28 18:45:22 +0100

    use broadcaster_login for twitch url instead of display_name

--- components/LiveStream.vue
+++ components/LiveStream.vue
@@ -2,15 +2,12 @@
 	<div class="card">
 		<div class="m-2">
 			<h4>{{ stream.title }}</h4>
-			<a :href="`https://www.twitch.tv/${stream.display_name}`" target="_blank">{{ stream.display_name }}</a>
+			<a :href="`https://www.twitch.tv/${stream.broadcaster_login}`" target="_blank">{{ stream.display_name }}</a>
 		</div>
 		<div class="aspect-ratio" v-aspect-ratio="'16:9'">
 			<iframe
-				:src="`https://player.twitch.tv/?channel=${stream.display_name}&parent=${clientUrl}&muted=true&autoplay=false`"
-				width="100%"
-				height="100%"
-				allowfullscreen="true"
-			></iframe>
+				:src="`https://player.twitch.tv/?channel=${stream.broadcaster_login}&parent=${clientUrl}&muted=true&autoplay=false`"
+				width="100%" height="100%" allowfullscreen="true"></iframe>
 		</div>
 	</div>
 </template>
